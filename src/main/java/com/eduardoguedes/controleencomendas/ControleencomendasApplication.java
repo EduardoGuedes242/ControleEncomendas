5c5d69e3d1d1f3e7fcccad26d28ce98307b19c7f
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-08-24 23:49:45 -0300

    first commit

---
+++ src/main/java/com/eduardoguedes/controleencomendas/ControleencomendasApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoguedes.controleencomendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleencomendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleencomendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardoguedes/controleencomendas/ControleencomendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoguedes.controleencomendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleencomendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
